d205a5133165da2dc12c3ae6f279c558613c8318
ShyrkovaMaryna <sirkovamarina7@gmail.com>
2024-12-26 23:21:58 +0200

    Updated .gitignore file

---
+++ src/HomeWork2/Main.java
@@ -0,0 +1,10 @@
+package HomeWork2;
+
+public class Main {
+    private static final double CONV_M = 1.60934;
+
+    public static void main(String[] args) {
+
+        System.out.println("Converter project: Miles to Kilometers and Kilometers to Miles");
+    }
+}
