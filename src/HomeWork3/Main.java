3cdbc0db74a1bba8a3fca5bc2ef743bc49e25584
ShyrkovaMaryna <sirkovamarina7@gmail.com>
2024-12-30 15:29:53 +0200

    Add Fahrenheit to Celsius conversion

--- src/HomeWork3/Main.java
+++ src/HomeWork3/Main.java
@@ -3,5 +3,9 @@ package HomeWork3;
 public class Main {
     public static void main(String[] args){
         System.out.println("Converter Application: Fahrenheit to Celsius and Celsius to Fahrenheit.");
+        System.out.println( "100°F to Celsius: " + convertFahrenheitToCelsius(100));
+    }
+    public static double convertFahrenheitToCelsius(double fahrenheit){
+        return (fahrenheit -32) * 5/9;
     }
 }
