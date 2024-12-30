b1af17282175cd3c194c17fcfd4df900e2da78b4
ShyrkovaMaryna <sirkovamarina7@gmail.com>
2024-12-30 15:25:15 +0200

    Initial commit: Add project information

---
+++ src/HomeWork3/Main.java
@@ -0,0 +1,7 @@
+package HomeWork3;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Converter Application: Fahrenheit to Celsius and Celsius to Fahrenheit.");
+    }
+}
